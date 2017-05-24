5061f17e99765379a40a62da65b07634bcb72abb
BobdeK <appxbuild@AppXBuilds-Mac-mini.local>
2017-05-24 12:21:23 +0530

    Grid layout

---
+++ gridLayout/app/src/main/java/com/example/appxbuild/gridlayout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.appxbuild.gridlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import  android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
